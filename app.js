12e5f86783d4fe623dba5847e60e94224063dcef
Danial Alfayyadh <danialalfayyadh28@gmail.com>
2023-07-16 08:43:24 +0700

    fitur cart

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 // Database configuration
 const host = "localhost"; // Ubah jika perlu
@@ -35,6 +36,9 @@ app.set('views', path.join(__dirname, 'views'));
 // Set view engine to EJS
 app.set('view engine', 'ejs');
 
+//Set Body Parser 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -63,7 +67,54 @@ app.get('/menu', (req, res) => {
     });
   });
   
+  app.get('/add-to-cart', (req, res) => {
+    const menuId = req.query.menu_id;
+    const quantity = req.query.quantity;
   
+    // Insert the item into the cart table in the database
+    const sql = "INSERT INTO cart (menu_id, quantity) VALUES (?, ?)";
+    connection.query(sql, [menuId, quantity], (err, result) => {
+      if (err) {
+        console.error('Error adding item to cart: ' + err.stack);
+        return;
+      }
+    
+      res.redirect('/cart'); // Redirect to the cart page after adding the item
+    });
+  });
+
+  app.post('/remove-from-cart', (req, res) => {
+    const cartId = req.body.cart_id;
+  
+    // Delete the item from the cart table in the database
+    const sql = "DELETE FROM cart WHERE id = ?";
+    connection.query(sql, [cartId], (err, result) => {
+      if (err) {
+        console.error('Error removing item from cart: ' + err.stack);
+        return;
+      }
+    
+      res.redirect('/cart'); // Redirect to the cart page after removing the item
+    });
+  });
+  
+ 
+  app.get('/cart', (req, res) => {
+    // Retrieve cart items from the database
+    const sql = "SELECT c.id, m.name, m.description, c.quantity FROM cart c JOIN menu m ON c.menu_id = m.id";
+    connection.query(sql, (err, result) => {
+      if (err) {
+        console.error('Error retrieving cart items: ' + err.stack);
+        return;
+      }
+    
+      res.render('cart', { cartItems: result });
+    });
+  });
+  
+
+
+
 // Start the server
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
